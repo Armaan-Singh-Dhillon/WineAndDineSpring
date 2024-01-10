ba696810df7a9c82da36b9d86b863243b492b198
Armaan-Singh-Dhillon <dhillonarmaan2004.com>
2024-01-10 10:51:39 +0530

    complex structures separated

--- src/main/java/com/armaan/wineanddinebackend/Entity/Dish.java
+++ src/main/java/com/armaan/wineanddinebackend/Entity/Dish.java
@@ -1,12 +1,6 @@
 package com.armaan.wineanddinebackend.Entity;
 
 import jakarta.persistence.Entity;
-import java.util.*;
-
-import com.armaan.wineanddinebackend.TypeClasses.Ingredients;
-import com.armaan.wineanddinebackend.TypeClasses.OffersAndSales;
-import com.armaan.wineanddinebackend.TypeClasses.Options;
-import com.armaan.wineanddinebackend.TypeClasses.Reviews;
 
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -24,19 +18,14 @@ public class Dish {
     private int deliveryTime;
     private int fatContent;
     private int protein;
-    private Ingredients ingredients;
-    private Options options;
-    private Reviews reviews;
-    private List<OffersAndSales> salesAndOffers;
-    private List<String> tags;
+
     private String type;
 
     public Dish() {
     }
 
     public Dish(int id, String name, double price, String image, String description, int averageTimeToCook,
-            int deliveryTime, int fatContent, int protein, Ingredients ingredients, Options options, Reviews reviews,
-            List<OffersAndSales> salesAndOffers, List<String> tags, String type) {
+            int deliveryTime, int fatContent, int protein, String type) {
         this.id = id;
         this.name = name;
         this.price = price;
@@ -46,11 +35,6 @@ public class Dish {
         this.deliveryTime = deliveryTime;
         this.fatContent = fatContent;
         this.protein = protein;
-        this.ingredients = ingredients;
-        this.options = options;
-        this.reviews = reviews;
-        this.salesAndOffers = salesAndOffers;
-        this.tags = tags;
         this.type = type;
     }
 
@@ -126,46 +110,6 @@ public class Dish {
         this.protein = protein;
     }
 
-    public Ingredients getIngredients() {
-        return ingredients;
-    }
-
-    public void setIngredients(Ingredients ingredients) {
-        this.ingredients = ingredients;
-    }
-
-    public Options getOptions() {
-        return options;
-    }
-
-    public void setOptions(Options options) {
-        this.options = options;
-    }
-
-    public Reviews getReviews() {
-        return reviews;
-    }
-
-    public void setReviews(Reviews reviews) {
-        this.reviews = reviews;
-    }
-
-    public List<OffersAndSales> getSalesAndOffers() {
-        return salesAndOffers;
-    }
-
-    public void setSalesAndOffers(List<OffersAndSales> salesAndOffers) {
-        this.salesAndOffers = salesAndOffers;
-    }
-
-    public List<String> getTags() {
-        return tags;
-    }
-
-    public void setTags(List<String> tags) {
-        this.tags = tags;
-    }
-
     public String getType() {
         return type;
     }
@@ -178,9 +122,7 @@ public class Dish {
     public String toString() {
         return "Dish [id=" + id + ", name=" + name + ", price=" + price + ", image=" + image + ", description="
                 + description + ", averageTimeToCook=" + averageTimeToCook + ", deliveryTime=" + deliveryTime
-                + ", fatContent=" + fatContent + ", protein=" + protein + ", ingredients=" + ingredients + ", options="
-                + options + ", reviews=" + reviews + ", salesAndOffers=" + salesAndOffers + ", tags=" + tags + ", type="
-                + type + "]";
+                + ", fatContent=" + fatContent + ", protein=" + protein + ", type=" + type + "]";
     }
 
 }
