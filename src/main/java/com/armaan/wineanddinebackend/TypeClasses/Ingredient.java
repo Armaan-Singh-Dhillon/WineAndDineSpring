b47293fcee2000aa28fe69833844e06ad57465aa
Armaan-Singh-Dhillon <dhillonarmaan2004@gmail.com>
2024-01-14 17:20:33 +0530

    Ingredient Dish model Implemented

--- src/main/java/com/armaan/wineanddinebackend/TypeClasses/Ingredient.java
+++ src/main/java/com/armaan/wineanddinebackend/TypeClasses/Ingredient.java
@@ -1,35 +1,25 @@
 package com.armaan.wineanddinebackend.TypeClasses;
 
+import com.armaan.wineanddinebackend.Entity.Dish;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Entity
+@Data
 public class Ingredient {
+    @Id
+    private int ingredient_id;
     private String name;
     private String description;
+    private String category;
+    @ManyToOne
+    @JoinColumn(name = "dish_id")
+    @JsonBackReference
+    private Dish dish;
 
-    public Ingredient(String name, String description) {
-        this.name = name;
-        this.description = description;
-    }
-
-    public Ingredient() {
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    @Override
-    public String toString() {
-        return "Ingredient [name=" + name + ", description=" + description + "]";
-    }
 }
--- src/main/java/com/armaan/wineanddinebackend/controllers/DishController.java
+++ src/main/java/com/armaan/wineanddinebackend/controllers/DishController.java
@@ -1,14 +1,10 @@
 package com.armaan.wineanddinebackend.controllers;
 
-import java.util.Optional;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +12,7 @@ import com.armaan.wineanddinebackend.Entity.Dish;
 import com.armaan.wineanddinebackend.Service.DatabaseService;
 
 @RestController
-@RequestMapping("/api/yourEntities")
+@RequestMapping("/")
 public class DishController {
     private final DatabaseService service;
 
@@ -25,15 +21,10 @@ public class DishController {
         this.service = service;
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Optional<Dish>> getEntityById(@PathVariable int id) {
-        Optional<Dish> entity = service.repository.findById(id);
-        return ResponseEntity.ok(entity);
+    @GetMapping("/")
+    public List<Dish> getEntityById() {
+        List<Dish> entity = service.repository.findAll();
+        return entity;
     }
 
-    @PostMapping
-    public ResponseEntity<Dish> createEntity(@RequestBody Dish entity) {
-        Dish createdEntity = service.repository.save(entity);
-        return ResponseEntity.status(HttpStatus.CREATED).body(createdEntity);
-    }
 }
