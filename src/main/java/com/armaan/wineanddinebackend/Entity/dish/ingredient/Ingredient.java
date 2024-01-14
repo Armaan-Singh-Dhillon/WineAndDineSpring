c3a3d01cd8c6362730a7ef51808feb195f7c630f
Armaan-Singh-Dhillon <dhillonarmaan2004@gmail.com>
2024-01-14 17:20:35 +0530

    options entitiy inroduced and primitive data modelling-completed

--- src/main/java/com/armaan/wineanddinebackend/Entity/dish/ingredient/Ingredient.java
+++ src/main/java/com/armaan/wineanddinebackend/Entity/dish/ingredient/Ingredient.java
@@ -1,12 +1,7 @@
 package com.armaan.wineanddinebackend.Entity.dish.ingredient;
 
-import com.armaan.wineanddinebackend.Entity.dish.Dish;
-import com.fasterxml.jackson.annotation.JsonBackReference;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 @Entity
@@ -17,9 +12,5 @@ public class Ingredient {
     private String name;
     private String description;
     private String category;
-    @ManyToOne
-    @JoinColumn(name = "dish_id")
-    @JsonBackReference
-    private Dish dish;
 
 }
