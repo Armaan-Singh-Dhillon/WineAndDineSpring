a4206e58797b318e4ac3f80be229f5acdf17c063
Armaan-Singh-Dhillon <dhillonarmaan2004@gmail.com>
2024-01-14 17:20:32 +0530

    initial commit

---
+++ src/main/java/com/armaan/wineanddinebackend/WineAndDineBackendApplication.java
@@ -0,0 +1,13 @@
+package com.armaan.wineanddinebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineAndDineBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WineAndDineBackendApplication.class, args);
+    }
+
+}
